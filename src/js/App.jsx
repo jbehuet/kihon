20df5d92c3ab96d942c3e017fa7d3281770c6c33
jbehuet <jbehuet@gmail.com>
2017-03-15 23:59:56 +0100

    move sw

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -40,6 +40,6 @@ ReactDOM.render(
 
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-             .register('./sw.js')
+             .register('sw.js')
              .then(function() { console.log('Service Worker Registered'); });
   }
