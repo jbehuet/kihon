d68c1c56d6e335993771b7d9da3af1025f4f2446
jbehuet <jbehuet@gmail.com>
2017-03-15 18:19:03 +0100

    Edit deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,26 +5,21 @@ echo "Deploying"
 echo "----------------------"
 
 # Ensuring npm deps are installed
-#npm install
+npm install
 
 # Cleaning dist directory
-#rm -rf dist/
-
-# Cloning deploy branch into dist directory
-#git clone -b deploy https://github.com/jbehuet/aikido.git dist/
+rm -rf dist/
 
 # Build
-#npm run build
+npm run build
 
 # Copy all public files
-#cp -R public/* dist/
+cp -r public/* dist/
 
-# Checking in dist/
-#cd dist
+
 # Adding files, removing missing files (including files with spaces as well)
-#git add .
-#if [ $(git ls-files --deleted | wc -l) -ne 0 ]; then git ls-files --deleted | sed -e 's/^/"/g' -e 's/$/"/g' | xargs git rm; fi;
-#git commit -m "Auto-deploy"
+git add -f dist
+git commit -m "Auto-deploy"
 
 # Pushing to deploy branch
-#if [ $(ls -al | wc -l) -ge 10 ]; then git push origin deploy; else echo "Avoiding any push since there is an empty dist/ dir"; fi;
+git subtree push --prefix dist origin master
