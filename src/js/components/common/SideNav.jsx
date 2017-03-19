7dc8448911c28173d009f7812a74bfbaeecb8775
jbehuet <jbehuet@gmail.com>
2017-03-19 20:38:10 +0100

    Change checkbox to radio

--- src/js/components/common/SideNav.jsx
+++ src/js/components/common/SideNav.jsx
@@ -1,9 +1,8 @@
 import React, {Component} from 'react';
 import Drawer from 'material-ui/Drawer';
 import Divider from 'material-ui/Divider';
-import Checkbox from 'material-ui/Checkbox';
+import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton';
 import MenuItem from 'material-ui/MenuItem';
-import RaisedButton from 'material-ui/RaisedButton';
 
 const styles = {
     header: {
@@ -20,7 +19,7 @@ const styles = {
         padding: '0px 16px',
         lineHeight: '48px'
     },
-    checkbox: {
+    radio: {
         marginLeft: '16px'
     },
     button: {
@@ -46,12 +45,14 @@ class SideNav extends Component {
                 <MenuItem onTouchTap={this.handleChange.bind(this)}>Accueil</MenuItem>
                 <Divider/>
                 <MenuItem disabled={true}>Filtre</MenuItem>
-                <Checkbox label="5e Kyu" checkedIcon={<img src='images/belts/5e_kyu.png' width="24"/>} uncheckedIcon={<img src='images/belts/6e_kyu.png' width="24"/>} style={styles.checkbox}/>
-                <Checkbox label="4e Kyu" checkedIcon={<img src='images/belts/4e_kyu.png' width="24"/>} uncheckedIcon={<img src='images/belts/6e_kyu.png' width="24"/>} style={styles.checkbox}/>
-                <Checkbox label="3e Kyu" checkedIcon={<img src='images/belts/3e_kyu.png' width="24"/>} uncheckedIcon={<img src='images/belts/6e_kyu.png' width="24"/>} style={styles.checkbox}/>
-                <Checkbox label="2e Kyu" checkedIcon={<img src='images/belts/2e_kyu.png' width="24"/>} uncheckedIcon={<img src='images/belts/6e_kyu.png' width="24"/>} style={styles.checkbox}/>
-                <Checkbox label="1e Kyu" checkedIcon={<img src='images/belts/1e_kyu.png' width="24"/>} uncheckedIcon={<img src='images/belts/6e_kyu.png' width="24"/>} style={styles.checkbox}/>
-                <RaisedButton label="Appliquer" primary={true} style={styles.button}/>
+                <RadioButtonGroup name="belts" defaultSelected="0">
+                    <RadioButton value="5" label="5e Kyu" checkedIcon={< img src = 'images/belts/5e_kyu.png' width = "24" />} uncheckedIcon={< img src = 'images/belts/6e_kyu.png' width = "24" />} style={styles.radio}/>
+                    <RadioButton value="4" label="4e Kyu" checkedIcon={< img src = 'images/belts/4e_kyu.png' width = "24" />} uncheckedIcon={< img src = 'images/belts/6e_kyu.png' width = "24" />} style={styles.radio}/>
+                    <RadioButton value="3" label="3e Kyu" checkedIcon={< img src = 'images/belts/3e_kyu.png' width = "24" />} uncheckedIcon={< img src = 'images/belts/6e_kyu.png' width = "24" />} style={styles.radio}/>
+                    <RadioButton value="2" label="2e Kyu" checkedIcon={< img src = 'images/belts/2e_kyu.png' width = "24" />} uncheckedIcon={< img src = 'images/belts/6e_kyu.png' width = "24" />} style={styles.radio}/>
+                    <RadioButton value="1" label="1e Kyu" checkedIcon={< img src = 'images/belts/1e_kyu.png' width = "24" />} uncheckedIcon={< img src = 'images/belts/6e_kyu.png' width = "24" />} style={styles.radio}/>
+                    <RadioButton value="0" label="1e Dan" checkedIcon={< img src = 'images/belts/1e_dan.png' width = "24" />} uncheckedIcon={< img src = 'images/belts/6e_kyu.png' width = "24" />} style={styles.radio}/>
+                </RadioButtonGroup>
                 <Divider/>
                 <MenuItem>A propos</MenuItem>
                 <div style={styles.footer}>© 2017 - Images propriétées de la FFAAA</div>
