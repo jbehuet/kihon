1f5d5e67caf85f0bf30d70c7749c022cd1c1e087
jbehuet <jbehuet@gmail.com>
2017-10-02 16:57:18 +0200

    Edit deploy

--- deploy.sh
+++ deploy.sh
@@ -1,31 +1,43 @@
-#!/bin/bash -x
+#!/bin/bash
 
-echo "----------------------"
-echo "Deploying"
-echo "----------------------"
+# Reset
+RCol='\033[0m'       # Text Reset
 
-# Ensuring npm deps are installed
-npm install
+# Regular Colors
+Bla='\033[0;30m';
+Red='\033[0;31m';
+Gre='\033[0;32m';
+Yel='\033[0;33m';
+Blu='\033[0;34m';
+Pur='\033[0;35m';
+Cya='\033[0;36m';
+Whi='\033[0;37m';
 
-# Cleaning dist directory
-rm -rf dist/
-mkdir dist/
-
-# Cloning master branch (GH PAGES) into dist folder
-git clone -b master https://github.com/jbehuet/aikido.git dist/
+echo -e "${Yel}------------"
+echo -e " DEPLOYING"
+echo -e "------------ ${RCol}"
 
 # Build
+echo -e "* Build : ${Gre}(1/4)${RCol}"
 npm run build
-cp -r public/* dist/
 
 # Adding new files
-cd dist/
-git add .
+echo -e "* Add / Commit ${Gre}(2/4)${RCol}"
+git add dist/ --force
 git commit -m "Auto-deploy"
 
 # Pushing to deploy branch
-git push origin master $1
+echo '* Push ${Gre}(3/4)${RCol}'
+git subtree push --prefix dist origin master
+
+#git push origin `git subtree split --prefix dist master`:master --force
 
 # Cleaning
-cd ..
+echo -e "* Cleaning ${Gre}(4/4)${RCol}"
 rm -rf dist
+
+echo
+echo '----------------------------------------------------------'
+echo -e "Deployed to : ${Cya}https://jbehuet.github.io/aikido/#/${RCol}"
+echo '----------------------------------------------------------'
+
