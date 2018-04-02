eff7fd399a935e0b1c79bb7bd4dfeb8fcfaa606f
jbehuet <jbehuet@gmail.com>
2018-04-02 20:36:01 +0200

    Refacto Actions

--- src/app/actions/MovementsActions.js
+++ src/app/actions/MovementsActions.js
@@ -1,34 +1,33 @@
 export const Tabs = {
-    TECHNIQUES: 'Techniques',
-    ATTAQUES: 'Attaques'
-}
+  TECHNIQUES: 'Techniques',
+  ATTAQUES: 'Attaques',
+};
 
 export const Types = {
-    FETCH_ALL: 'FETCH_ALL',
-    FETCH_FILTRED: 'FETCH_FILTRED',
-    SELECT_TAB: 'SELECT_TAB',
-}
+  FETCH_ALL: 'FETCH_ALL',
+  FETCH_FILTRED: 'FETCH_FILTRED',
+  SELECT_TAB: 'SELECT_TAB',
+};
 
 export class Actions {
+  static fetchAll() {
+    return {
+      type: Types.FETCH_ALL,
+    };
+  }
 
-    static fetchAll() {
-        return {
-            type: Types.FETCH_ALL
-        }
-    }
+  static fetchFiltred(kyu) {
+    return {
+      type: Types.FETCH_FILTRED,
+      payload: { kyu },
+    };
+  }
 
-    static fetchFiltred(kyu) {
-        return {
-            type: Types.FETCH_FILTRED,
-            payload: { kyu }
-        }
-    }
-
-    static selectTab(tab) {
-        return {
-            type: Types.SELECT_TAB,
-            payload: { tab }
-        }
-    }
+  static selectTab(tab) {
+    return {
+      type: Types.SELECT_TAB,
+      payload: { tab },
+    };
+  }
 }
 
