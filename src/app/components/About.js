b346848c0a879fb7ba6c190af04d73841c1c74ff
jbehuet <jbehuet@gmail.com>
2018-04-02 20:35:24 +0200

    Update About

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -14,7 +14,10 @@ const styles = {
 const About = () => (
   <div style={styles.container}>
     <h2 style={styles.title}>A propos</h2>
-    <p>Cette application a été développée en ayant comme objectif de facilité l'apprentissage des différents termes issues des techniques de l'aikido.</p>
+    <p>
+      {"Cette application a été développée en ayant comme objectif de facilité \
+    l'apprentissage des différents termes issues des techniques de l'aikido."}
+    </p>
     <h2 style={styles.title}>Propriété intellectuelle</h2>
     <p>Cette application est sous licence MIT.</p>
     <p>Les images et dessins des techniques intégrées dans cette application sont la propriété de la <a href="http://www.aikido.com.fr/">FFAAA</a> et ne sauraient être reproduits librement sans demande préalable à la FFAAA et sans l’indication de la source. Il est possible de demander une autorisation de reproduction uniquement à des fins personnelles, associatives ou professionnelles, mais en aucun cas ne peut faire l’objet d’exploitation commerciale ou publicitaire.</p>
