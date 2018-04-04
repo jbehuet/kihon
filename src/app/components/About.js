3dcaadd3f362875aeca999b69ba21d51353f0e94
jbehuet <jbehuet@gmail.com>
2018-04-04 18:04:17 +0200

    Update about

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -21,10 +21,12 @@ const About = () => (
     <h2 style={styles.title}>Propriété intellectuelle</h2>
     <p>Cette application est sous licence MIT.</p>
     <p>Les images et dessins des techniques intégrées dans cette application sont la propriété de la <a href="http://www.aikido.com.fr/">FFAAA</a> et ne sauraient être reproduits librement sans demande préalable à la FFAAA et sans l’indication de la source. Il est possible de demander une autorisation de reproduction uniquement à des fins personnelles, associatives ou professionnelles, mais en aucun cas ne peut faire l’objet d’exploitation commerciale ou publicitaire.</p>
+    <p>Les informations relatives aux stages proviennent du site <a href="http://www.stages-aikido.fr/">stages-aikido.fr</a></p>
     <h2 style={styles.title}>Développement</h2>
     <p>Par <a href="http://jbehuet.fr">Jérôme BEHUET</a></p>
     <br />
     <img src="images/logo-ffaaa.png" alt="" />
+    <br />
   </div>
 );
 
