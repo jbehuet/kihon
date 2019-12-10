a65c10c18c56e4a684a5b53d226f92f231ae6f77
Jerome BEHUET <jbehuet@gmail.com>
2019-12-10 15:31:49 +0100

    update firebase

--- src/app/Messaging.js
+++ src/app/Messaging.js
@@ -3,7 +3,13 @@ import * as firebase from 'firebase/app';
 import 'firebase/messaging';
 
 const initializedFirebaseApp = firebase.initializeApp({
-  messagingSenderId: '431963400045',
+  apiKey: "AIzaSyBmqZMB5H0mLC2Jcgs1D2qrMgWd6hCj_bQ",
+  authDomain: "kihon-24459.firebaseapp.com",
+  databaseURL: "https://kihon-24459.firebaseio.com",
+  projectId: "kihon-24459",
+  storageBucket: "kihon-24459.appspot.com",
+  messagingSenderId: "431963400045",
+  appId: "1:431963400045:web:e67e7d049bed3485f45c64"
 });
 
 const messaging = initializedFirebaseApp.messaging();
--- src/static/firebase-messaging-sw.js
+++ src/static/firebase-messaging-sw.js
@@ -2,7 +2,13 @@ importScripts('https://www.gstatic.com/firebasejs/5.7.2/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/5.7.2/firebase-messaging.js');
 
 firebase.initializeApp({
-    'messagingSenderId': '431963400045'
+  'apiKey': "AIzaSyBmqZMB5H0mLC2Jcgs1D2qrMgWd6hCj_bQ",
+  'authDomain': "kihon-24459.firebaseapp.com",
+  'databaseURL': "https://kihon-24459.firebaseio.com",
+  'projectId': "kihon-24459",
+  'storageBucket': "kihon-24459.appspot.com",
+  'messagingSenderId': "431963400045",
+  'appId': "1:431963400045:web:e67e7d049bed3485f45c64"
 });
 
 const messaging = firebase.messaging();
